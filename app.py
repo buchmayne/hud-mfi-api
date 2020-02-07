a6530e4eb592ccc667e9c7d40e63c96ec47ef18e
buchmayne <michaelmbuchman@gmail.com>
2020-02-07 15:35:03 -0800

    changed routes to be state long for the full name, and added two additional routes to get county

--- app.py
+++ app.py
@@ -40,8 +40,8 @@ def get_by_state_code(state_code_):
         return(str(e))
 
 
-@app.route("/get/state_name/<state_name_>")
-def get_by_state_name(state_name_):
+@app.route("/get/state_long/<state_name_>")
+def get_by_state_long(state_name_):
     try:
         median_income = MFI.query.filter_by(state_name=state_name_).all()
         return jsonify([i.serialize() for i in median_income])
@@ -54,6 +54,28 @@ def get_by_state(state_):
     try:
         median_income = MFI.query.filter_by(state=state_).all()
         return jsonify([i.serialize() for i in median_income])
+    except Exception as e:
+        return(str(e))
+
+
+# add routes for specifying county name
+@app.route("/get/state_name/<state_name_>/county/<string:county_>")
+def get_by_state_long_and_county_name(state_name_, county_):
+    try:
+        county_long = '{} County'.format(county_)
+        median_income = MFI.query.filter_by(state_name=state_name_).filter_by(county=county_long).first()
+        return jsonify(median_income.serialize())
+    except Exception as e:
+        return(str(e))
+
+
+@app.route("/get/state/<state_>/county/<string:county_>")
+def get_by_state_and_county_name(state_, county_):
+    try:
+        county_long = '{} County'.format(county_)
+        median_income = MFI.query.filter_by(
+            state=state_).filter_by(county=county_long).first()
+        return jsonify(median_income.serialize())
     except Exception as e:
         return(str(e))
 
