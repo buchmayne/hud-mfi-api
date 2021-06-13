4f3c256296b622b9b3ce626f3d85b100b55fec83
buchmayne <michaelmbuchman@gmail.com>
2021-06-13 11:28:18 -0700

    back to old db string with just postgres prefix, and adding code to change the db string after being loaded

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config["DEBUG"] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL'].replace(
+    "://", "ql://", 1)
 db = SQLAlchemy(app)
 
 
