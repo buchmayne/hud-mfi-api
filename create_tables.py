97d1d00709f32109a2c913ce823763e2dd9373c4
buchmayne <michaelmbuchman@gmail.com>
2021-12-26 22:01:35 -0800

    succesfully writing all data from 2000 to 2021 to DB straight from HUD

--- create_tables.py
+++ create_tables.py
@@ -3,19 +3,6 @@ from sqlalchemy import create_engine
 from configparser import ConfigParser
 from sqlalchemy.engine.url import URL
 
-# load database connection parameters and create engine
-filename = 'database.ini'
-
-parser = ConfigParser()
-parser.read(filename)
-
-params = {}
-for k, v in parser.items('postgresql'):
-    params[k] = v
-
-
-conn = create_engine(URL(**params))
-
 # define paths to HUD INCOME LIMITS data
 mfi = {
     'raw_mfi_2021': "https://www.huduser.gov/portal/datasets/il/il21/Section8-FY21.xlsx",
@@ -26,27 +13,54 @@ mfi = {
     'raw_mfi_2016': "https://www.huduser.gov/portal/datasets/il/il16/Section8-FY16.xlsx",
     'raw_mfi_2015': "https://www.huduser.gov/portal/datasets/il/il15/Section8_Rev.xlsx",
     'raw_mfi_2014': "https://www.huduser.gov/portal/datasets/il/il14/Poverty.xls",
-    'raw_mfi_2013': "https://www.huduser.gov/portal/datasets/il/il13/Section8.xls"
+    'raw_mfi_2013': "https://www.huduser.gov/portal/datasets/il/il13/Section8.xls",
+    'raw_mfi_2012': "https://www.huduser.gov/portal/datasets/il/il12/Section8.xls",
+    'raw_mfi_2011': "https://www.huduser.gov/portal/datasets/il/il11/Section8_v3.xls",
+    'raw_mfi_2010': "https://www.huduser.gov/portal/datasets/il/il10/Section8.xls",
+    'raw_mfi_2009': "https://www.huduser.gov/portal/datasets/il/il09/Section8.xls",
+    'raw_mfi_2008': "https://www.huduser.gov/portal/datasets/il/il08/Section8_FY08.xls",
+    'raw_mfi_2007': "https://www.huduser.gov/portal/datasets/il/il07/Section8-rev.xls",
+    'raw_mfi_2006': "https://www.huduser.gov/portal/datasets/il/il06/Section8FY2006.xls",
+    'raw_mfi_2005': "https://www.huduser.gov/portal/datasets/il/il05/Section8FY2005.xls",
+    'raw_mfi_2004': "https://www.huduser.gov/portal/datasets/IL/IL04/Section8.xls",
+    'raw_mfi_2003': "https://www.huduser.gov/portal/datasets/IL/FMR03/Section8.xls",
+    'raw_mfi_2002': "https://www.huduser.gov/portal/datasets/il/fmr02/prts801_02.xls",
+    'raw_mfi_2001': "https://www.huduser.gov/portal/datasets/IL/FMR01/incfy01.xls",
+    'raw_mfi_2000': "https://www.huduser.gov/portal/datasets/IL/FMR00/incfy00.xls"
 }
 
 
-# write raw mfi
-def write_raw_hud_income_limits_to_db(tbl_name, hud_url):
+def create_connection_obj():
+    '''Creates engine object from database.ini configuration'''
+    filename = 'database.ini'
+    parser = ConfigParser()
+    parser.read(filename)
+
+    params = {k: v for k, v in parser.items('postgresql')}
+    conn = create_engine(URL(**params))
+    return conn
+
+
+def write_raw_hud_income_limits_to_db(con, tbl_name, hud_url):
     '''Read HUD income limits data with pandas and write to db
 
+    con : sqlalchemy.Engine()
+        connection engine from by sqlalchemy.create_engine
     tbl_name : str
         name to be used for database table
     hud_url : str
         link to the HUD income limits dataset
     '''
     df = pd.read_excel(hud_url)
-    df.columns = [x.lower() for x in df.columns]
-    df.to_sql(con=conn, name=tbl_name, index=False, if_exists='replace')
+    df.columns = [x.lower().replace('%', 'pct').replace(
+        '(', '_').replace(')', '_').replace(' ', '_') for x in df.columns]
+    df.to_sql(con=con, name=tbl_name, index=False, if_exists='replace')
     print(f"Successfully Added {tbl_name} to Database")
 
 
 if __name__ == '__main__':
+    conn = create_connection_obj()
     for k, v in mfi.items():
-        write_raw_hud_income_limits_to_db(tbl_name=k, hud_url=v)
+        write_raw_hud_income_limits_to_db(con=conn, tbl_name=k, hud_url=v)
 
     print("All HUD income limits data added to database")
