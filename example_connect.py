c3c28c31be47e3185681619cfe95d34f221b5e55
buchmayne <michaelmbuchman@gmail.com>
2021-12-29 14:36:42 -0800

    removed files that were only relevant to previous version of the app

--- example_connect.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from sqlalchemy import create_engine
-from sqlalchemy.engine.url import URL
-from preprocessing_settings import db
-
-conn = create_engine(URL(**db))
-mfi_data = pd.read_sql_table(table_name='mfi', con=conn)
-print(mfi_data)
